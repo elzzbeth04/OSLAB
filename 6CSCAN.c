e6e35a61fe453bb3e021f9d7e91c16c36af66790
Elizabeth Mariya Shibu <121534327+elzzbeth04@users.noreply.github.com>
2024-11-09 21:00:32 +0530

    Update 6CSCAN.c

--- 6CSCAN.c
+++ 6CSCAN.c
@@ -1,90 +1,90 @@
-// Online C compiler to run C program online
 #include <stdio.h>
 #include <stdlib.h>
+
 int main() {
-    // Write C code here
-   int RQ[100],initial,n,size,sum=0,i,j,move;
-   printf("Enter the number of Requests\n");
+    int RQ[100], initial, n, size, sum = 0, i, j, move;
+    printf("Enter the number of Requests\n");
     scanf("%d", &n);
     
     // Reading the request sequence
     printf("Enter the Requests sequence\n");
-    for (int i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) {
         scanf("%d", &RQ[i]);
     }
     printf("Enter the head \n");
-    scanf("%d",&initial);
+    scanf("%d", &initial);
     
-    printf("enter the disk size\n");
-    scanf("%d",&size);
+    printf("Enter the disk size\n");
+    scanf("%d", &size);
     
-    printf("enter the movemnt 0 or1 ");
-    scanf("%d",&move);
+    printf("Enter the movement (0 for left, 1 for right): ");
+    scanf("%d", &move);
     
-    for(i=0;i<n-1;i++)
-    {
-        for(j=0;j<n-i-1;j++)
-        {
-            if(RQ[j]>RQ[j+1])
-            {
+    // Sort the requests
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (RQ[j] > RQ[j + 1]) {
                 int temp = RQ[j];
-                RQ[j] = RQ[j+1];
-                RQ[j+1] = temp;
+                RQ[j] = RQ[j + 1];
+                RQ[j + 1] = temp;
             }
-            }
-    }
-    int index=0;
-    for(i=0;i<n;i++)
-    {
-       if(RQ[i]>initial) 
-       {
-           index=i;
-           break;
-       }
+        }
     }
     
-    if(move==1)
-    {
-        for (int i = index; i < n; i++){
-            sum+=abs(RQ[i]-initial);
-            initial=RQ[i];
+    int index = 0;
+    for (i = 0; i < n; i++) {
+        if (RQ[i] > initial) {
+            index = i;
+            break;
         }
-        //last 
-        if(initial<size-1)
-        {
-            sum=sum+abs(size-1-initial);
-            initial=size-1;
-        }
-        sum=sum+abs(size-1-0);
-        initial=0;
-        //lower values
-        for (int i = 0; i <index; i++) {
+    }
+    
+    if (move == 1) {
+        // Moving to the right first
+        for (i = index; i < n; i++) {
             sum += abs(RQ[i] - initial);
             initial = RQ[i];
         }
-    }
-    else
-    {
-        for(i=index-1;i>=0;i--)
-        {
-            sum=sum+abs(RQ[i]-initial);
-            initial=RQ[i];
-        }
-        if(initial<0)
-        {
-            sum=sum+abs(initial-0);
-            initial=0;
-        }
-        printf("f");
-        sum=sum+abs(size-1-initial);
-        initial=size-1;
         
-        for ( i = n-1; i < index; i--) {
+        // Go to the end of the disk
+        if (initial < size - 1) {
+            sum += abs(size - 1 - initial);
+            initial = size - 1;
+        }
+        
+        // Wrap around to the start of the disk
+        sum += abs(size - 1 - 0);
+        initial = 0;
+        
+        // Service the requests on the left side
+        for (i = 0; i < index; i++) {
+            sum += abs(RQ[i] - initial);
+            initial = RQ[i];
+        }
+    } else {
+        // Moving to the left first
+        for (i = index - 1; i >= 0; i--) {
+            sum += abs(RQ[i] - initial);
+            initial = RQ[i];
+        }
+        
+        // Go to the start of the disk
+        if (initial > 0) {
+            sum += abs(initial - 0);
+            initial = 0;
+        }
+        
+        // Wrap around to the end of the disk
+        sum += abs(size - 1 - 0);
+        initial = size - 1;
+        
+        // Service the requests on the right side
+        for (i = n - 1; i >= index; i--) {
             sum += abs(RQ[i] - initial);
             initial = RQ[i];
         }
     }
-    printf("total head movements %d",sum);
-    }
     
-   
+    printf("Total head movements: %d\n", sum);
+    return 0;
+}
