72b57927d53074a07f981a47b39f6a25e338b665
Elizabeth Mariya Shibu <121534327+elzzbeth04@users.noreply.github.com>
2024-11-09 20:59:44 +0530

    Update 11FCFS.c

--- 11FCFS.c
+++ 11FCFS.c
@@ -1,56 +1,62 @@
-// Online C compiler to run C program online
 #include <stdio.h>
 
- struct process{
+struct process {
     int pid;
     int at;
     int bt;
     int ct;
     int tat;
     int wt;
-}p[10];
+} p[10];
+
 int main() {
-    // Write C code here
-   int n,i;
-    
+    int n, i;
+    float awt = 0.0, atat = 0.0;
+
     printf("Enter the number of processes: ");
     scanf("%d", &n);
-for (int i = 0; i < n; i++) {
+
+    for (i = 0; i < n; i++) {
         p[i].pid = i + 1;
         printf("Enter arrival time for process %d: ", i + 1);
         scanf("%d", &p[i].at);
         printf("Enter burst time for process %d: ", i + 1);
         scanf("%d", &p[i].bt);
     }
+
+    // Sort processes by arrival time (FCFS)
     struct process temp;
-    for (i = 0; i < n - 1; i++)
-    {
-        for (j = 0; j < n - 1 - i; j++)
-        {
-            if (p[j].btime > p[j + 1].btime)
-            {
+    for (i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1 - i; j++) {
+            if (p[j].at > p[j + 1].at) {
                 temp = p[j];
                 p[j] = p[j + 1];
                 p[j + 1] = temp;
             }
         }
     }
-    int current=0;
-    for(i=0;i<n;i++)
-    {
-        current=current+p[i].bt;
-        p[i].ct=current;
-        p[i].tat=p[i].ct-p[i].at;
-        p[i].wt=p[i].tat-p[i].bt;
-    }
-    float awt=0.0;
-    float atat=0.0;
-    printf("PID AT  BT  CT  TAT  WT\n");
-    for(i=0;i<n;i++)
-    {
-        printf("%d%5d%5d%5d%5d%5d\n",p[i].pid,p[i].at,p[i].bt,p[i].ct,p[i].tat,p[i].wt);
-    }
-    printf("Average Waiting Time = %f\n", awt / n);
-    printf("Average Turnaround Time = %f\n", atat / n);
 
+    int current = 0;
+    for (i = 0; i < n; i++) {
+        if (current < p[i].at) {
+            current = p[i].at;  // If CPU is idle, wait until process arrives
+        }
+        current += p[i].bt;
+        p[i].ct = current;
+        p[i].tat = p[i].ct - p[i].at;
+        p[i].wt = p[i].tat - p[i].bt;
+
+        // Accumulate total waiting and turnaround times
+        awt += p[i].wt;
+        atat += p[i].tat;
+    }
+
+    printf("PID  AT  BT  CT  TAT  WT\n");
+    for (i = 0; i < n; i++) {
+        printf("%2d%5d%5d%5d%5d%5d\n", p[i].pid, p[i].at, p[i].bt, p[i].ct, p[i].tat, p[i].wt);
+    }
+    printf("Average Waiting Time = %.2f\n", awt / n);
+    printf("Average Turnaround Time = %.2f\n", atat / n);
+
+    return 0;
 }
